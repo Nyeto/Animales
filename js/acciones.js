1de4a1fa9651adbd75c16addfe3e39cded94f7d1
Nyeto <nyeto@hotmail.com>
2016-05-01 21:02:13 -0500

    ver 1.1
    cambio de diagonal en audio

--- js/acciones.js
+++ js/acciones.js
@@ -5,18 +5,18 @@ $('#principal').height($('#page').height());//calcular el alto de la pantalla de
 	document.addEventListener("deviceready",function(){
 	//precarga el sonido
 	audio=window.plugins.LowLatencyAudio;
-	audio.preloadFX('caballo','audio\caballo.mp3',function(){},function(e){alert('Error '+e);});
-	audio.preloadFX('cerdo','audio\cerdo.mp3',function(){},function(e){alert('Error '+e);});
-	audio.preloadFX('chivo','audio\chivo.mp3',function(){},function(e){alert('Error '+e);});
-	audio.preloadFX('gallo','audio\gallo.mp3',function(){},function(e){alert('Error '+e);});
-	audio.preloadFX('ganzos','audio\ganzos.mp3',function(){},function(e){alert('Error '+e);});
-	audio.preloadFX('perro','audio\perro.mp3',function(){},function(e){alert('Error '+e);});
-	audio.preloadFX('elefante','audio\elefante.mp3',function(){},function(e){alert('Error '+e);});
-	audio.preloadFX('leon','audio\leon.mp3',function(){},function(e){alert('Error '+e);});
-	audio.preloadFX('puma','audio\puma.mp3',function(){},function(e){alert('Error '+e);});
-	audio.preloadFX('rana','audio\rana.mp3',function(){},function(e){alert('Error '+e);});
-	audio.preloadFX('gorila','audio\gorila.mp3',function(){},function(e){alert('Error '+e);});
-	audio.preloadFX('monos','audio\monos.mp3',function(){},function(e){alert('Error '+e);});
+	audio.preloadFX('caballo','audio/caballo.mp3',function(){},function(e){alert('Error '+e);});
+	audio.preloadFX('cerdo','audio/cerdo.mp3',function(){},function(e){alert('Error '+e);});
+	audio.preloadFX('chivo','audio/chivo.mp3',function(){},function(e){alert('Error '+e);});
+	audio.preloadFX('gallo','audio/gallo.mp3',function(){},function(e){alert('Error '+e);});
+	audio.preloadFX('ganzos','audio/ganzos.mp3',function(){},function(e){alert('Error '+e);});
+	audio.preloadFX('perro','audio/perro.mp3',function(){},function(e){alert('Error '+e);});
+	audio.preloadFX('elefante','audio/elefante.mp3',function(){},function(e){alert('Error '+e);});
+	audio.preloadFX('leon','audio/leon.mp3',function(){},function(e){alert('Error '+e);});
+	audio.preloadFX('puma','audio/puma.mp3',function(){},function(e){alert('Error '+e);});
+	audio.preloadFX('rana','audio/rana.mp3',function(){},function(e){alert('Error '+e);});
+	audio.preloadFX('gorila','audio/gorila.mp3',function(){},function(e){alert('Error '+e);});
+	audio.preloadFX('monos','audio/monos.mp3',function(){},function(e){alert('Error '+e);});
 
 	
 	//reproducir las notas
